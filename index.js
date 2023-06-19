ea2cd8bb4fd99c1df448b79854545df46bb9d4f0
Dmitry Korovin <d.korovin@alt-it.solutions>
2023-06-19 09:04:53 +0300

    feat: update project

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
-const fs = require("fs");
 const jsonServer = require("json-server");
 const path = require("path");
-const cors = require('cors')
+const https = require("https");
+const http = require("http");
+const fs = require("fs");
 
-const PORT = 8000;
+const IS_DEV = process.env.MODE === "development";
 
 const server = jsonServer.create();
 
 const router = jsonServer.router(path.resolve(__dirname, "db.json"));
 
 server.use(jsonServer.defaults({}));
-server.use(cors());
 server.use(jsonServer.bodyParser);
+
+// Нужно для небольшой задержки, чтобы запрос проходил не мгновенно, имитация реального апи
+if (IS_DEV) {
+	server.use(async (req, res, next) => {
+		await new Promise((res) => {
+			setTimeout(res, 800);
+		});
+		next();
+	});
+}
 
 // Эндпоинт для логина
 server.post("/login", (req, res) => {
@@ -45,7 +55,23 @@ server.use((req, res, next) => {
 
 server.use(router);
 
-// запуск сервера
-server.listen(PORT, () => {
-	console.log("server is running on 8000 port");
-});
+// настраиваем и запускаем https или http сервер в зависимости от окружения
+if (IS_DEV) {
+	const HTTP_PORT = 8000;
+	const httpServer = http.createServer(server);
+
+	httpServer.listen(HTTP_PORT, () => {
+		console.log(`http server is running on ${HTTP_PORT} port`);
+	});
+} else {
+	const privateKey = fs.readFileSync("/etc/letsencrypt/live/fsd-blog.ru-0003/privkey.pem", "utf8");
+	const certificate = fs.readFileSync("/etc/letsencrypt/live/fsd-blog.ru-0003/fullchain.pem", "utf8");
+	const credentials = { key: privateKey, cert: certificate };
+	const PORT = 8443;
+	const httpsServer = https.createServer(credentials, server);
+
+	httpsServer.listen(PORT, () => {
+		console.log(`https server is running on ${PORT} port`);
+	});
+
+}
